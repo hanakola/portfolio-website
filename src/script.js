fac6b0ff9fb5091b48adca4078da728cf7185a8a
Hana K <66429579+hanakola@users.noreply.github.com>
2020-08-29 21:48:55 +0100

    Changed navbar to off-canvas sidenav

---
+++ src/script.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("sidenav").style.width = "250px";
+  document.getElementById("container").style.marginRight = "250px";
+  document.body.style.backgroundColor = "lightgrey";
+}
+
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+  document.getElementById("container").style.marginRight = "0";
+  document.body.style.backgroundColor = "white";
+}
